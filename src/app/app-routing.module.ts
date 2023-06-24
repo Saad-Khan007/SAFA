687d47f8ad213cc4d8553dce6dd2a0a2f706f67f
Saad-Khan007 <saadkhan322417@gmail.com>
2023-06-24 05:56:06 +0500

    home build June 24,2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  footerBoxes: any[] = [
+    {
+      title: 'MY ACCOUNT',
+      items: [
+        'About Us',
+        'Delivery Information',
+        'Privacy Policy',
+        'Terms & Conditions',
+        'Newsletter'
+      ]
+    },
+    {
+      title: 'INFORMATION',
+      items: [
+        'My Account',
+        'Order History',
+        'Wish List',
+        'Specials',
+        'Brands'
+      ]
+    },
+    {
+      title: 'CUSTOMER SERVICE',
+      items: [
+        'Contact Us',
+        'Site Map',
+        'Gift Certificates',
+        'Affiliate',
+        'Returns'
+      ]
+    },
+    {
+      title: 'CONTACT US',
+      items: [
+        'Address: Unknown',
+        'Lahore',
+        'Call: 123-456-789',
+        'Email: safa@weapon.com'
+      ]
+    }
+  ];
 
+  externalLinks: string[] = [
+    'OpticsPlanet',
+    'AR500 Armor',
+    '5.11 Tactical',
+    'Tactical Gear Junkie',
+    'Cheaper Than Dirt',
+    'Police Gear',
+    'Tactical Distributors',
+    'GunMag Warehouse',
+    'Gun Safes',
+    'Alien Gear Holsters',
+    'US Patriot Tactical',
+    'Primary Arms',
+    'Bravo Company USA',
+    'Gun Pro Deals'
+  ];
 }
+
